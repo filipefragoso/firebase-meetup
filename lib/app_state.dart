d89a52057e48f9dfe6381945ef35230a522ed17a
Filipe Augusto de Souza Fragoso <>
2024-05-08 17:22:20 -0300

    Code improvement

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'guest_book_message.dart';                        // new
 
+enum Attending { yes, no, unknown }
+
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
@@ -17,6 +19,13 @@ class ApplicationState extends ChangeNotifier {
 
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
+  int _attendees = 0;
+  int get attendees => _attendees;
+  bool _emailVerified = false;
+
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+  Attending get attending => _attending;
 
   StreamSubscription<QuerySnapshot>? _guestBookSubscription;
   List<GuestBookMessage> _guestBookMessages = [];
@@ -30,32 +39,24 @@ class ApplicationState extends ChangeNotifier {
       EmailAuthProvider(),
     ]);
     
-    FirebaseAuth.instance.userChanges().listen((user) {
-      if (user != null) {
-        _loggedIn = true;
-        _guestBookSubscription = FirebaseFirestore.instance
-            .collection('guestbook')
-            .orderBy('timestamp', descending: true)
-            .snapshots()
-            .listen((snapshot) {
-          _guestBookMessages = [];
-          for (final document in snapshot.docs) {
-            _guestBookMessages.add(
-              GuestBookMessage(
-                name: document.data()['name'] as String,
-                message: document.data()['text'] as String,
-              ),
-            );
-          }
-          notifyListeners();
-        });
+    _addAttendingListener();
+    _addUserListeners();
+  }
+
+  set attending(Attending attending) {
+    var user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return;
+    }
+
+    final userDoc = FirebaseFirestore.instance
+      .collection('attendees')
+      .doc(user.uid);
+      if (attending == Attending.yes) {
+        userDoc.set(<String, dynamic> { 'attending': true } );
       } else {
-        _loggedIn = false;
-        _guestBookMessages = [];
-        _guestBookSubscription?.cancel();
+        userDoc.set(<String, dynamic> { 'attending': false } );
       }
-      notifyListeners();
-    });
   }
 
   Future<DocumentReference> addMessageToGuestBook(String message) {
@@ -74,4 +75,70 @@ class ApplicationState extends ChangeNotifier {
     });
   }
 
+  void _addAttendingListener() {
+    FirebaseFirestore.instance
+      .collection('attendees')
+      .where('attending', isEqualTo: true)
+      .snapshots()
+      .listen((snapshot) { 
+        _attendees = snapshot.docs.length;
+      });
+  }
+
+  void _addUserListeners(){
+    FirebaseAuth.instance.userChanges().listen((user) {
+      if (user != null) {
+        _emailVerified = user.emailVerified;
+        _loggedIn = true;
+
+        _addGuestBookListener(user);
+        _addUserAttendee(user);
+      } else {
+        _loggedIn = false;
+        _emailVerified = false;
+        _guestBookMessages = [];
+        _guestBookSubscription?.cancel();
+        _attendingSubscription?.cancel(); // new
+      }
+      notifyListeners();
+    });
+  }
+
+  void _addGuestBookListener(User user){
+    _guestBookSubscription = FirebaseFirestore.instance
+        .collection('guestbook')
+        .orderBy('timestamp', descending: true)
+        .snapshots()
+        .listen((snapshot) {
+      _guestBookMessages = [];
+      for (final document in snapshot.docs) {
+        _guestBookMessages.add(
+          GuestBookMessage(
+            name: document.data()['name'] as String,
+            message: document.data()['text'] as String,
+          ),
+        );
+      }
+      notifyListeners();
+    });
+  }
+
+  void _addUserAttendee(User user) {
+    _attendingSubscription = FirebaseFirestore.instance
+    .collection('attendees')
+    .doc(user.uid)
+    .snapshots()
+    .listen((snapshot) {
+      if (snapshot.data() != null) {
+        if (snapshot.data()!['attending'] as bool) {
+          _attending = Attending.yes;
+        } else {
+          _attending = Attending.no;
+        }
+      } else {
+        _attending = Attending.unknown;
+      }
+      notifyListeners();
+    });
+  }
 }
